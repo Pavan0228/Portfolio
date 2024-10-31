03d9dd26a033f299f36118a1b723ddf366f5c300
Pavan-0228 <pavanrasal4@gmail.com>
2024-10-31 18:06:22 +0530

    fix: correct import paths for motion utilities in multiple components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
-import { fadeIn, textVariant } from "../utils/motion";
+import { fadeIn, textVariant } from "../Utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt 
